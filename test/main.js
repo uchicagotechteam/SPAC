ff9577fb5410987b531b368a095e3a1692a62eea
Dan Solnik <dsolnik@cs.uchicago.edu>
2018-04-09 18:27:09 -0500

    got slider working

--- test/main.js
+++ test/main.js
@@ -1,37 +1,47 @@
+var valVLengthOfStay = 0;
+var valVIncomingPrisoners = 0;
+var valNVLengthOfStay = 0;
+var valNVIncomingPrisoners = 0;
+
+function update() {
+		console.log('Violent lengthOfStay', valVLengthOfStay);
+		console.log('Violent Incoming Prisoners', valVIncomingPrisoners);
+		console.log('NonViolent lengthOfStay', valNVLengthOfStay);
+		console.log('Nonviolent incomingPrisoners', valNVIncomingPrisoners);
+}
+
 function createSlider(name) {
-	$('#' + name).slider({
-		formatter: function (value) {
-				return 'Current value: ' + value;
-		}
-});
+		$('#' + name).slider({
+				formatter: function (value) {
+						return 'Current value: ' + value;
+				},
+				change: update
+		});
 
 }
 
-$(document).ready(function() {
-	createSlider('ex1');
-	createSlider('ex2');
-	createSlider('ex3');
-	createSlider('ex4');
+$(document)
+		.ready(function () {
+				createSlider('VLengthOfStay');
+				createSlider('VIncomingPrisoners');
+				createSlider('NVLengthOfStay');
+				createSlider('NVIncomingPrisoners');
 
-	var slider1 = $("#ex1").slider();
-	console.log(slider1.slider('getValue'));
-	var slider2 = $("#ex2").slider();
-	console.log(slider2.slider('getValue'));
-	var slider3 = $("#ex3").slider();
-	console.log(slider3.slider('getValue'));
-	var slider4 = $("#ex4").slider();
-	console.log(slider4.slider('getValue'));
+				var VLengthOfStay = $("#VLengthOfStay")
+						.slider()
+						.on('slideStop', update);
+				var VIncomingPrisoners = $("#VIncomingPrisoners")
+						.slider()
+						.on('slideStop', update);
+				var NVLengthOfStay = $("#NVLengthOfStay")
+						.slider()
+						.on('slideStop', update);
+				var NVIncomingPrisoners = $("#NVIncomingPrisoners")
+						.slider()
+						.on('slideStop', update);
 
-	
-$('#push').click(function() {
-	var slider1 = $("#ex1").slider();
-	console.log(slider1.slider('getValue'));
-	var slider2 = $("#ex2").slider();
-	console.log(slider2.slider('getValue'));
-	var slider3 = $("#ex3").slider();
-	console.log(slider3.slider('getValue'));
-	var slider4 = $("#ex4").slider();
-	console.log(slider4.slider('getValue'));
-});
-
-});
+				valVLengthOfStay = VLengthOfStay.slider('getValue');
+				valVIncomingPrisoners = VIncomingPrisoners.slider('getValue');
+				valNVLengthOfStay = NVLengthOfStay.slider('getValue');
+				valNVIncomingPrisoners = NVLengthOfStay.slider('getValue');
+		});
