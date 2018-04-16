7afa7c9f5d179f6299e3da9c9d45ce6e9d4ad717
Colin Flaherty <cflaherty@cs.uchicago.edu>
2018-04-16 18:12:07 -0500

    added percent function to grid.js

--- SPAC_DotGrid/grid.js
+++ SPAC_DotGrid/grid.js
@@ -2,6 +2,7 @@ console.log("LOADED");
 
 var width = 500;
 var height = 500;
+var total_dots = 500*500/(25*25);
 
 var grid = d3.select("body")
    .append("svg")
@@ -10,8 +11,8 @@ var grid = d3.select("body")
 
 
 
-for (var i=25; i <= width-25; i=i+25) {
-	for (var j=25; j <= height-25; j=j+25) {
+for (var j=25; j <= height-25; j+=25) {
+	for (var i=25; i <= width-25; i+=25) {
     	grid.append("circle")
     	    .attr("class", "circle")
         	.attr("cx", i)
@@ -26,5 +27,8 @@ function colorNDotsAtM (m,n, color) {
  grid.selectAll(".circle").filter(function(d, i) {return (i >= m) && (i < m+n);}).style("fill", color);
 }
 
-colorNDotsAtM(0,40,"red");
-colorNDotsAtM(40,100,"blue");
+function colorPercentageNatM (percFilled, percToFill, color) {
+  colorNDotsAtM(floor(percFilled*total_dots),floor(percToFill*total_dots),color);
+  var percNowFilled = percFilled+percToFill;
+  return percNowFilled;
+}
